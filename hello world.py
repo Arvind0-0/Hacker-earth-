d5be2bf2c8e617a71cc879ea64510267c8fc5416
43avin <91238742+43avin@users.noreply.github.com>
2022-02-05 13:30:39 +0530

    Create hello world.py

---
+++ hello world.py
@@ -0,0 +1,29 @@
+'''Problem
+Read different types of data from standard input, process them as shown in output format and print the answer to standard output.
+
+Input format:
+First line contains integer N.
+Second line contains string S.
+
+Output format:
+First line should contain N x 2.
+Second line should contain the same string S.
+
+Constraints:
+
+ where S length of string S
+
+Sample Input
+5
+helloworld
+Sample Output
+10
+helloworl
+'''
+
+
+# Write your code here
+n = int(input())
+j = input()
+print(n*2)
+print(j)
